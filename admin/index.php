58c3665d82a3f20e0b3a11142d4f798d69342a60
Nhật Mặt Trăng <nhatcaca2004@gmail.com>
2023-11-09 23:23:23 +0700

    giaodien

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,19 @@
 <?php
 session_start();
+include("view/header.php");
 
-include("header.php");
-include("menu.php");
-
-if (isset($_GET["act"]) && $_GET["act"]) {
-    $act = $_GET["act"];
-    switch ($act) {
-        case 'themdanhmuc':
-            include('danhmuc/them.php');
-            break;
+// if (isset($_GET["act"]) && $_GET["act"]) {
+//     $act = $_GET["act"];
+//     switch ($act) {
+//         case 'themdanhmuc':
+//             include('danhmuc/them.php');
+//             break;
 
 
-    }
-} else {
-    include("main.php");
-}
-include("footer.php");
+//     }
+// } else {
+// }
+
+include("view/main.php");
+include("view/footer.php");
 ?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+include("views/header.php");
+if (isset($_GET["act"]) && $_GET["act"]) {
+    $act = $_GET["act"];
+    switch ($act) {
+        case 'themdanhmuc':
+            // include('danhmuc/them.php');
+            break;
+
+
+    }
+} else {
+    include("views/main.php");
+}
+include("views/footer.php");
+?>
